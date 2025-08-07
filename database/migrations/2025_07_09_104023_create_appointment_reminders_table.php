66b0ff5a323e3dda0a1286372c00e9a748f0d72b
Emin Azeroglu <azeroglu.developer@gmail.com>
2025-08-07 18:32:09 +0400

    fix: #ID-31 - Clinic, Doctor, Patient, Review modules

--- database/migrations/2025_07_09_104023_create_appointment_reminders_table.php
+++ database/migrations/2025_07_09_104023_create_appointment_reminders_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
             // $table->index(['appointment_id', 'type', 'scheduled_at', 'is_sent']);
 			$table->index(
 				['appointment_id', 'type', 'scheduled_at', 'is_sent'],
-				'appt_reminder_idx' // Custom name under 64 chars
+				'appt_reminder_idx' // Custom name under 64 charssss
 			);
         });
     }
